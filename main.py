bc111fb959d3b7c6f17ce4902483693947b8dd9a
Gustavo Valentin <gstvalentin@hotmail.com>
2022-04-11 14:50:19 -0300

    function verification

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('creu')
+aves = input('Uma ave: ')
+lugar = input('Lugar: ')
+animal = input('Um animal: ')
+outroLugar = input('outro lugar: ')
+
+print(f'Certo dia todas as aves foram convidadas para uma festa no {lugar}.\n \
+O {animal} não foi convidado e ficou chateado, mas ele teve uma ideia \n \
+podia ir à festa pegando carona no violão do {aves}. No dia seguinte, \n \
+sem que o {aves} percebesse o {animal} entrou no violão e ficou quietinho. \n \
+“Que engraçado, este violão está pesado “, pensou o {aves}, e voou pelo {lugar}. \n \
+Chegando ao {lugar}, o {aves} colocou o violão num canto da sala e o {animal} saiu. \n \
+Que surpresa para as {aves} a presença de um {animal} na festa! \n \
+Todos queriam saber como ele tinha chegado, mas ele só dizia: \n \
+- É segredo , é segredo. \n \
+As {aves} se reuniram e resolveram expulsar o {animal} dizendo: \n \
+- Você não é {aves}, não foi convidado e tem a boca grande, vamos jogá-lo lá {outroLugar}. \n \
+Assim o fizeram e o {animal} caiu de costas na água sem se machucar. \n \
+O {animal} aprendeu a lição e nunca mais foi numa festa sem ser antes convidado.')
---
+++ modules.py
@@ -0,0 +1,5 @@
+def festaNoCeu(aves, lugar, animal, outroLugar):
+    if aves[-1] == 'o':
+        return 
+    elif aves[-1] == 'a':
+        return None
